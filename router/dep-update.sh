c2e02f02bf415beab1a486e07a44293222b95cc0
slaiyer <slaiyer@users.noreply.github.com>
2022-05-17 21:29:14 +0530

    set src as default dir for dependency updates

--- router/dep-update.sh
+++ router/dep-update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "${1:-tests}" \
+cd "${1:-src}" \
     && . venv/bin/activate \
     && python3 -m pip install --upgrade pip \
     && python3 -m pip install --upgrade pip-tools \
