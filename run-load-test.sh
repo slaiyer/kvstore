c75e8469dc20f1ce940116c33d8b3ced8d93b409
slaiyer <slaiyer@users.noreply.github.com>
2022-05-19 21:14:24 +0530

    fix typo

--- run-load-test.sh
+++ run-load-test.sh
@@ -7,7 +7,7 @@ num_conn="${4:-10}"
 qps="${5:-500}"
 duration="${6:-30s}"
 
-mkdir -p "${data-dir}"
+mkdir -p "${data_dir}"
 fortio load -quiet -data-dir "${data_dir}" -a -c "${num_conn}" -qps "${qps}" -t "${duration}" -content-type 'application/json' -payload '{"key":"abc-1","value":"1"}' "http://${host}:${port}/set" &
 fortio load -quiet -data-dir "${data_dir}" -a -c "${num_conn}" -qps "${qps}" -t "${duration}" "http://${host}:${port}/get/abc-1" &
 fortio load -quiet -data-dir "${data_dir}" -a -c "${num_conn}" -qps "${qps}" -t "${duration}" "http://${host}:${port}/search?prefix=abc&suffix=-1" &
