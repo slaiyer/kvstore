3a01950a624c3ddb8d6798b60e0247b193752cb1
slaiyer <slaiyer@users.noreply.github.com>
2022-05-19 20:37:10 +0530

    clean up old test code/fixtures

--- router/local-ip.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker network ls | rg redisnet | cut -d' ' -f1 | xargs -I_ docker inspect _ | jq -r ".[0].Containers | .[] | select(.Name==\"${1:-router}\") | .IPv4Address" | cut -d'/' -f1
